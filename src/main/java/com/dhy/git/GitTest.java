529a05cb9d4aae6983506ffdaed1ae1e52f4a682
Dhy <Dhy@qq.com>
2022-08-06 09:06:46 +0800

    first commit

---
+++ src/main/java/com/dhy/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.dhy.git;
+
+/**
+ * @author dhystart
+ * @create 2022-08-06 9:02
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
